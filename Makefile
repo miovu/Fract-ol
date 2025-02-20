798bbe494c1c1f50ada3a5eab96029af4016074e
Margarida Iovu <miovu@c2r7s4.42lisboa.com>
2025-02-20 17:58:56 +0000

    fract_ol.h

---
+++ Makefile
@@ -0,0 +1,52 @@
+NAME = fract_ol
+
+CC = cc
+CFLAGS = -Wall -Werror -Wextra -g
+MLXFLAGS = -Lmlx_linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz
+
+SOURCES_FILES = create_window.c
+				
+SRC = $(addprefix $(SRC_DIR)/, $(SOURCES_FILES))
+
+OBJECTS = $(addprefix $(OBJECTS_DIR)/, $(SOURCES_FILES:.c=.o))
+OBJECTS_DIR = objects
+SRC_DIR = srcs
+
+LIBFT = $(LIBFT_DIR)/libft.a
+LIBFT_DIR = ./includes/libft
+
+MLX = $(MLX_DIR)/libmlx.a
+MLX_DIR = ./minilibx-linux
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(MLX) $(OBJECTS) 
+	@echo "Building..."
+	@$(CC) $(CFLAGS) $(OBJECTS) $(LIBFT) $(MLX) $(MLXFLAGS) -o $(NAME)
+	@echo "Ready!"
+
+$(OBJECTS_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJECTS_DIR)
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT):
+	@make -C $(LIBFT_DIR) bonus -s --no-print-directory
+
+$(MLX):
+	@make -C $(MLX_DIR)
+
+$(OBJECTS_DIR):
+	@mkdir -p $(OBJECTS_DIR)
+
+clean:
+	@rm -rf $(OBJECTS_DIR)
+	@make -C $(LIBFT_DIR) clean -s
+	@echo "Cleaned!"
+
+fclean: clean
+	@make -C $(LIBFT_DIR) fclean -s --no-print-directory
+	@rm -rf $(NAME)
+
+re: fclean all
+	@echo "Rebuilded!"
+
+.PHONY: all clean fclean re
---
+++ includes/libft/ft_lstnew.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miovu <miovu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 18:45:30 by miovu             #+#    #+#             */
+/*   Updated: 2025/02/13 18:29:40 by miovu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+/* #include <stdio.h>
+int	main()
+{
+	t_list *test = ft_lstnew("Hello world");
+
+	printf("List: %s", (char *)test->content);
+} */
